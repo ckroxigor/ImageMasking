1fde6dc06c778b98d3b0c5ca9f2fe96ef8afc48e
ckroxigor <ckroxigor@gmail.com>
2014-06-17 19:02:59 +0200

    Letters like y and g are not cut anymore.

--- js/main.js
+++ js/main.js
@@ -2,34 +2,38 @@
 // Handle the configuration of the SVG
 function ImgConf($scope) {
 	$scope.src_url = "http://www.w3.org/html/logo/downloads/HTML5_Logo_512.png",
-	$scope.mask_text = "Hello World!!!",
-	$scope.bgcolor = [0, 0, 0],	//In RGB
+	$scope.mask_text = "01",
+	$scope.bgcolor = [255, 255, 255],	//In RGB
 	$scope.font_size = 2,
 
-	//format the color for the SVG
+	//formats the color for the SVG
 	$scope.getBgColor = function () {
 		return "rgb(" + $scope.bgcolor[0] + "," + $scope.bgcolor[1] + "," + $scope.bgcolor[2] + ")"
 	},
 
-
-	$scope.getImgURL = function () {
-		return $scope.src_url;
-	},
-
+	// Decreases the font size
 	$scope.reduceFontSize = function () {
 		if ($scope.font_size > 1) 
 			$scope.font_size--;
 	},
 
+	// Increases the font size
 	$scope.increaseFontSize = function() {
 		$scope.font_size++;
 	},
 
+	// Returns the width of the text pattern
 	$scope.getTextWidth = function() {
 		return $('#svgHiddenText').get(0).getBBox()['width'];
 	}
+
+	// Returns the height of the text pattern
+	$scope.getTextHeight = function() {
+		return $('#svgHiddenText').get(0).getBBox()['height'];
+	}
 }
 
+//Start angular application
 var app = angular.module('app', []);
 
 //Borrowed from: http://plnkr.co/edit/cMhGRh?p=preview
